12bb6f4810d3fe566a56954d843cca0daa86f764
Samad <faroos3@rpi.edu>
2018-02-14 22:11:19 -0500

    adding the stuff

---
+++ mantisbt-2.11.1/config/config_inc.php
@@ -0,0 +1,10 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = '';
+
+$g_default_timezone       = 'America/New_York';
+
+$g_crypto_master_salt     = 'hfoLm7XVPwdQnhXl0URaDoU082Ix8brte70zYQ3SjSE=';
